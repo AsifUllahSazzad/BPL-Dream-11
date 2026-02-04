00be73d148cb7bc03d77e96cb54100e0552fdfa6
Asif Ullah Sazzad <asifullahsazzad0@gmail.com>
2026-02-04 18:11:45 +0600

    Update Subscribe component

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,6 +1,20 @@
+import { useState } from 'react';
 import './Subscribe.css'
 
 const Subscribe = () => {
+
+    const [value, setValue] = useState("");
+
+    const handleTheValue = () =>{
+        
+        if(value.includes("@")){
+            console.log(value)
+        }
+        else{
+            console.log("error")
+        }
+    }
+
     return (
         <div className='rounded-3xl border-2 border-white p-6 bg-[#ffffff15]'>
 
@@ -9,8 +23,13 @@ const Subscribe = () => {
                 <p className='newFont text-xl text-[#13131370] font-medium'>Get the latest updates and news right in your inbox!</p>
 
                 <div className='flex gap-x-4'>
-                    <input className='text-base px-7.5 py-4.5 rounded-xl inset-shadow-2xs border border-[#E5E5E5] w-100' type="email" placeholder='Enter your email'/>
-                    <button className='btnStyle text-base font-bold px-7.5 rounded-xl shadow-[inset_0px_10px_10px_rgba(0,0,0,0.1),inset_-10px_0px_10px_rgba(0,0,0,0.1),inset_0px_-10px_10px_rgba(0,0,0,0.1),inset_10px_0px_10px_rgba(0,0,0,0.1)]'>Subscribe</button>
+                    <input
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    className='text-base px-7.5 py-4.5 rounded-xl inset-shadow-2xs border border-[#E5E5E5] w-100' type="email" placeholder='Enter your email'/>
+                    <button
+                    onClick={handleTheValue}
+                    className='btnStyle text-base font-bold px-7.5 rounded-xl shadow-[inset_0px_10px_10px_rgba(0,0,0,0.1),inset_-10px_0px_10px_rgba(0,0,0,0.1),inset_0px_-10px_10px_rgba(0,0,0,0.1),inset_10px_0px_10px_rgba(0,0,0,0.1)]'>Subscribe</button>
                 </div>
             </div>
 
