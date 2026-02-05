a4f4017f9902fcb7d7030b2b7b5d8ace7e696d24
Asif Ullah Sazzad <asifullahsazzad0@gmail.com>
2026-02-06 00:10:42 +0600

    Update Subscribe component

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -3,24 +3,41 @@ import './Subscribe.css'
 const Subscribe = ({handleTheValue, setValue, value}) => {
 
     return (
-        <div className='rounded-3xl border-2 border-white p-6 bg-[#ffffff15]'>
+        <div className="rounded-3xl border-2 border-white  bg-[#ffffff15] md:p-6 p-4 max-md:mx-5">
 
-            <div className='newsletter rounded-3xl flex flex-col justify-center items-center gap-y-4 border border-[#d6dddd]'>
-                <h3 className='font-bold text-3xl'>Subscribe to our Newsletter</h3>
-                <p className='newFont text-xl text-[#13131370] font-medium'>Get the latest updates and news right in your inbox!</p>
+  <div className="newsletter rounded-3xl flex flex-col justify-center items-center gap-y-4 border border-[#d6dddd] text-center p-6">
 
-                <div className='flex gap-x-4'>
-                    <input
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                    className='text-base px-7.5 py-4.5 rounded-xl inset-shadow-2xs border border-[#E5E5E5] w-100' type="email" placeholder='Enter your email'/>
-                    <button
-                    onClick={handleTheValue}
-                    className='btnStyle text-base font-bold px-7.5 rounded-xl shadow-[inset_0px_10px_10px_rgba(0,0,0,0.1),inset_-10px_0px_10px_rgba(0,0,0,0.1),inset_0px_-10px_10px_rgba(0,0,0,0.1),inset_10px_0px_10px_rgba(0,0,0,0.1)]'>Subscribe</button>
-                </div>
-            </div>
+    <h3 className="font-bold text-2xl md:text-3xl">
+      Subscribe to our Newsletter
+    </h3>
 
-        </div>
+    <p className="newFont text-base md:text-xl text-[#13131370] font-medium">
+      Get the latest updates and news right in your inbox!
+    </p>
+
+    {/* Input + Button */}
+    <div className="flex flex-col sm:flex-row gap-3 w-full max-w-md">
+
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type="email"
+        placeholder="Enter your email"
+        className="w-full text-base px-5 py-3 md:px-7 md:py-4 rounded-xl border border-[#E5E5E5] inset-shadow-2xs"
+      />
+
+      <button
+        onClick={handleTheValue}
+        className="btnStyle w-full sm:w-auto text-base font-bold px-6 py-3 md:px-7 rounded-xl
+        shadow-[inset_0px_10px_10px_rgba(0,0,0,0.1),inset_-10px_0px_10px_rgba(0,0,0,0.1),inset_0px_-10px_10px_rgba(0,0,0,0.1),inset_10px_0px_10px_rgba(0,0,0,0.1)]"
+      >
+        Subscribe
+      </button>
+
+    </div>
+  </div>
+</div>
+
     );
 };
 
